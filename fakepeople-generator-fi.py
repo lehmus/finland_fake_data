3bddda70319208c960be84bedca3a1cea2aef179
lehmus <16114036+lehmus@users.noreply.github.com>
2020-05-27 01:18:16 +0300

    Refactoring

--- fakepeople-generator-fi.py
+++ fakepeople-generator-fi.py
@@ -1,11 +1,12 @@
-import random
+import argparse
 import csv
+import logging
 import os
+import random
+import sys
 
-print('Starting...')
 
-class finnish_people_generator:
-    main_data_dir = 'c:\\fakedata-fi'  # Change based on your file location
+class FinnishPersonGenerator:
 
     # Weighted name picker lists
     #  wsl = weighted select list
@@ -16,94 +17,115 @@ class finnish_people_generator:
     names_male_first = []
     names_female_first = []
     names_last = []
-    
-    
+
+
+    def __init__(self, main_data_dir):
+
+        # Assign properties
+        self.__main_data_dir = main_data_dir
+        # Load data
+        self.load_people_objects()
+        # Initiate random seed for generator functions
+        random.seed()
+
+
     def load_people_objects(self):
+
         # Male first (given) names
-        male_fn_file = os.sep.join((self.main_data_dir,'csv','etunimitilasto-2020-02-06-miehet.csv'))
-        with open(male_fn_file,encoding='utf8') as infile:
+        male_fn_file = os.sep.join((self.__main_data_dir, 'person-names/csv', 'etunimitilasto-2020-02-06-miehet.csv'))
+        with open(male_fn_file, encoding='utf8') as infile:
             inreader = csv.reader(infile)
             next(inreader)  # skip the header row
             ctr = 0
             for row in inreader:
-                (fname,weight) = (row[0],int(row[1].replace(",","")))
+                (fname,weight) = (row[0],int(row[1].replace(',', '')))
                 self.names_male_first.append(fname)
                 self.weights_male_first.append(weight)
                 ctr += 1
 
         # Female first (given) names
-        female_fn_file = os.sep.join((self.main_data_dir,'csv','etunimitilasto-2020-02-06-naiset.csv'))
-        with open(female_fn_file,encoding='utf8') as infile:
+        female_fn_file = os.sep.join((self.__main_data_dir, 'person-names/csv', 'etunimitilasto-2020-02-06-naiset.csv'))
+        with open(female_fn_file, encoding='utf8') as infile:
             inreader = csv.reader(infile)
             next(inreader)  # skip the header row
             ctr = 0
             for row in inreader:
-                (fname,weight) = (row[0],int(row[1].replace(",","")))
+                (fname,weight) = (row[0],int(row[1].replace(',', '')))
                 self.names_female_first.append(fname)
                 self.weights_female_first.append(weight)
                 ctr += 1
 
         # Last (family) names
-        family_name_file = os.sep.join((self.main_data_dir,'csv','sukunimitilasto-2020-02-06.csv'))
-        with open(family_name_file,encoding='utf8') as infile:
+        family_name_file = os.sep.join((self.__main_data_dir, 'person-names/csv', 'sukunimitilasto-2020-02-06.csv'))
+        with open(family_name_file, encoding='utf8') as infile:
             inreader = csv.reader(infile)
             next(inreader)  # skip the header row
             ctr = 0
             for row in inreader:
-                (lname,weight) = (row[0],int(row[1].replace(",","")))
+                (lname,weight) = (row[0],int(row[1].replace(',', '')))
                 self.names_last.append(lname)
                 self.weights_last.append(weight)
                 ctr += 1
 
+
     def get_a_name(self, gender=None):
-        """
+        '''
         Returns a name in (given,family) tuple.
         Randomly picks a gender if none is specified
         Gender is in English - "m" or "f"
-        """
+        '''
+
         lname = random.choices(self.names_last, self.weights_last)[0]
         fname = None
         if gender is None:
-            gender = random.choice(['f','m'])
+            gender = random.choice(['f', 'm'])
         if gender == 'm':
             fname = random.choices(self.names_male_first, self.weights_male_first)[0]
         else:
             fname = random.choices(self.names_female_first, self.weights_female_first)[0]
-        
+
         return((fname,lname))
 
 
-    def __init__(self):
-        random.seed()
-        self.load_people_objects()
-                                 
+class FinnishAddressGenerator:
 
-class finnish_addresses_generator:
-    main_data_dir = 'c:\\fakedata-fi'
-    
     postal_code_to_city = {}
     postal_code_addresses = {}  # {post code: [(street,unit,lat,long)....] }
     population_by_postal_code = {}  # {post code: (total pop, male pop, female pop)}
     
+
+    def __init__(self, main_data_dir):
+
+        # Assign properties
+        self.__main_data_dir = main_data_dir
+        # Load data
+        self.load_postal_code_city()
+        self.load_population_stats()
+        # Initiate random seed for generator functions
+        random.seed()
+
+
     def load_postal_code_city(self):
-        file_loc = os.sep.join((self.main_data_dir,'posti','PCF_20200523.dat'))
-        with open(file_loc) as infile:    # This file is ANSI encoded
+        file_loc = os.sep.join((self.__main_data_dir, 'posti', 'PCF_20200523.dat'))
+        with open(file_loc, encoding='latin-1') as infile:    # This file is ANSI encoded
             for row in infile.readlines():
                 postcode = row[13:18] # Fixed width file
                 city_fi = row[179:199].strip()
                 self.postal_code_to_city[postcode] = city_fi
     
+
     def load_population_stats(self):
-        file_loc = os.sep.join((self.main_data_dir,'csv','001_12ey_2018.csv'))
+        file_loc = os.sep.join((self.__main_data_dir, 'population-stats', '001_12ey_2018.csv'))
         with open(file_loc, encoding='utf8') as infile:
             csv_reader = csv.reader(infile)
             next(csv_reader)
             for row in csv_reader:
-                (postal_code_area,total_population,male_population,female_population) = row
+                (postal_code_area, total_population, male_population, female_population) = row
                 postal_code = postal_code_area[0:5]
                 # Right now, this just uses total population
                 self.population_by_postal_code[postal_code] = int(total_population)
         
+
     def load_postal_code_addresses(self, postal_code):
         # To save memory, this procedure loads addresses by the first number of a postal code.
         # If you call this procedure and the necessary posal code is already loaded - then it just does nothing
@@ -112,19 +134,20 @@ class finnish_addresses_generator:
         if dict_length > 0 and keys[0][0] == postal_code[0]:
             return None
         self.postal_code_addresses = {}
-        file_loc = os.sep.join((self.main_data_dir,'openaddr-collected-europe','fi','countrywide-fi.csv'))
+        file_loc = os.sep.join((self.__main_data_dir, 'openaddr-collected-europe', 'fi', 'countrywide-fi.csv'))
         if not os.path.exists(file_loc):
             raise Exception('\n***You will need to download address files from openaddresses.io.  Please see the openaddr-collected-europe subdirectory for details.')
         with open(file_loc, encoding='utf8') as infile:
             csv_reader = csv.reader(infile)
             next(csv_reader)
             for row in csv_reader:
-                (long,lat,addr_number,street,unit,city,district,region,postcode,rowid,rowhash) = row
+                (long, lat,addr_number, street, unit, city, district, region, postcode, rowid, rowhash) = row
                 if postcode[0] == postal_code[0]:
                     # Add it to the dictionary
                     if postcode not in self.postal_code_addresses:
                         self.postal_code_addresses[postcode] = []
-                    self.postal_code_addresses[postcode].append((street,addr_number,long,lat))
+                    self.postal_code_addresses[postcode].append((street, addr_number, long, lat))
+
 
     def get_address_in_postal_code(self,postal_code):
         # The postal code population file is only good at 4-digit granularity
@@ -145,31 +168,82 @@ class finnish_addresses_generator:
         else:
             return random.choice(self.postal_code_addresses[chosen_code])
     
-    def __init__(self):
-        random.seed()
-        self.load_postal_code_city()
-        self.load_population_stats()
 
-outdir = 'c:\\fakedata-fi\\output'
+def get_arguments(args, arg_defs):
+    '''
+    Read and parse program arguments.
+    '''
 
-fp = finnish_people_generator()
-fa = finnish_addresses_generator()
-
-people_per_postcode = fa.population_by_postal_code
-
-postcode_keys = sorted(people_per_postcode.keys())
-
-with open(os.sep.join((outdir,'fakepeople_fi.csv')),mode='w',encoding='utf8', newline='') as ow: # change the newline if using Linux
-    cw = csv.writer(ow)
-    for postcode in postcode_keys:
-        print('Processing {0}'.format(postcode))
-        population = people_per_postcode[postcode]
-        sample_rate = .05
-        for i in range(int(population * sample_rate)):
-            (first_name,last_name) = fp.get_a_name()
-            (street,unit,lat,long) = fa.get_address_in_postal_code(postcode)
-            city = fa.postal_code_to_city[postcode]
-            row = (first_name,last_name,street,unit,city,postcode,lat,long)
-            cw.writerow(row)
+    parser = argparse.ArgumentParser()
+    for arg_name in arg_defs.keys():
+        short_flag = '-' + arg_defs[arg_name]['short_flag']
+        long_flag = '--' + arg_name
+        default_value = arg_defs[arg_name]['default_value']
+        arg_type = arg_defs[arg_name]['type']
+        description = arg_defs[arg_name]['description']
+        parser.add_argument(
+            short_flag, long_flag, dest=arg_name, type=arg_type,
+            default=default_value, help=description
+        )
+    args = parser.parse_args(args[1:])
+    return args
 
 
+def main(argv):
+
+    output_filename = 'fakepeople_fi.csv'
+
+    # Define and read program input arguments
+    program_args = {
+        'data_dir': {
+            'short_flag': 'd',
+            'type': str,
+            'default_value': 'data',
+            'description': 'Data directory path on the host system.'
+        },
+        'output_dir': {
+            'short_flag': 'o',
+            'type': str,
+            'default_value': 'output',
+            'description': 'Output directory path on the host system.'
+        },
+        'log_level': {
+            'short_flag': 'l',
+            'type': str,
+            'default_value': 'warning',
+            'description': 'Logging output level.'
+        }
+    }
+    args = get_arguments(argv, program_args)
+
+    # Set up logging
+    log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    log_level = getattr(logging, args.log_level.upper())
+    logging.basicConfig(level=log_level, format=log_format)
+
+    fp = FinnishPersonGenerator(main_data_dir=args.data_dir)
+    fa = FinnishAddressGenerator(main_data_dir=args.data_dir)
+
+    people_per_postcode = fa.population_by_postal_code
+
+    postcode_keys = sorted(people_per_postcode.keys())
+
+    output_file = os.sep.join((args.output_dir, output_filename))
+    logging.info('Beginning output to ' + output_file)
+    with open(output_file, mode='w', encoding='utf8', newline='') as ow: # change the newline if using Linux
+        cw = csv.writer(ow)
+        for postcode in postcode_keys:
+            print('Processing {0}'.format(postcode))
+            population = people_per_postcode[postcode]
+            sample_rate = .05
+            for i in range(int(population * sample_rate)):
+                (first_name, last_name) = fp.get_a_name()
+                (street, unit, lat, long) = fa.get_address_in_postal_code(postcode)
+                city = fa.postal_code_to_city[postcode]
+                row = (first_name, last_name, street, unit, city, postcode, lat, long)
+                cw.writerow(row)
+
+
+if __name__ == '__main__':
+
+    main(sys.argv)
