1651e48c34b96faf9e3ad6f3699058dde35e3c57
lehmus <16114036+lehmus@users.noreply.github.com>
2020-05-28 00:21:14 +0300

    - Refactor class properties - Add retrieval functions for properties - Improved parameterization - Improve logging

--- fakepeople-generator-fi.py
+++ fakepeople-generator-fi.py
@@ -8,25 +8,22 @@ import sys
 
 class FinnishPersonGenerator:
 
-    # Weighted name picker lists
-    #  wsl = weighted select list
-    weights_male_first = []
-    weights_female_first = []
-    weights_last = []
-    # dict = dictionaries (in list form) that map numbers in the wsl to actual names
-    names_male_first = []
-    names_female_first = []
-    names_last = []
-
-
     def __init__(self, main_data_dir):
 
         # Assign properties
         self.__main_data_dir = main_data_dir
-        # Load data
-        self.load_people_objects()
+        # Weighted name picker lists (wsl = weighted select list)
+        self.__weights_male_first = []
+        self.__weights_female_first = []
+        self.__weights_last = []
+        # dict = dictionaries (in list form) that map numbers in the wsl to actual names
+        self.__names_male_first = []
+        self.__names_female_first = []
+        self.__names_last = []
         # Initiate random seed for generator functions
         random.seed()
+        # Load data
+        self.load_people_objects()
 
 
     def load_people_objects(self):
@@ -39,8 +36,8 @@ class FinnishPersonGenerator:
             ctr = 0
             for row in inreader:
                 (fname,weight) = (row[0],int(row[1].replace(',', '')))
-                self.names_male_first.append(fname)
-                self.weights_male_first.append(weight)
+                self.__names_male_first.append(fname)
+                self.__weights_male_first.append(weight)
                 ctr += 1
 
         # Female first (given) names
@@ -51,8 +48,8 @@ class FinnishPersonGenerator:
             ctr = 0
             for row in inreader:
                 (fname,weight) = (row[0],int(row[1].replace(',', '')))
-                self.names_female_first.append(fname)
-                self.weights_female_first.append(weight)
+                self.__names_female_first.append(fname)
+                self.__weights_female_first.append(weight)
                 ctr += 1
 
         # Last (family) names
@@ -63,8 +60,8 @@ class FinnishPersonGenerator:
             ctr = 0
             for row in inreader:
                 (lname,weight) = (row[0],int(row[1].replace(',', '')))
-                self.names_last.append(lname)
-                self.weights_last.append(weight)
+                self.__names_last.append(lname)
+                self.__weights_last.append(weight)
                 ctr += 1
 
 
@@ -75,34 +72,39 @@ class FinnishPersonGenerator:
         Gender is in English - "m" or "f"
         '''
 
-        lname = random.choices(self.names_last, self.weights_last)[0]
+        lname = random.choices(self.__names_last, self.__weights_last)[0]
         fname = None
         if gender is None:
             gender = random.choice(['f', 'm'])
         if gender == 'm':
-            fname = random.choices(self.names_male_first, self.weights_male_first)[0]
+            fname = random.choices(self.__names_male_first, self.__weights_male_first)[0]
         else:
-            fname = random.choices(self.names_female_first, self.weights_female_first)[0]
+            fname = random.choices(self.__names_female_first, self.__weights_female_first)[0]
 
         return((fname,lname))
 
 
 class FinnishAddressGenerator:
 
-    postal_code_to_city = {}
-    postal_code_addresses = {}  # {post code: [(street,unit,lat,long)....] }
-    population_by_postal_code = {}  # {post code: (total pop, male pop, female pop)}
-    
-
     def __init__(self, main_data_dir):
 
         # Assign properties
         self.__main_data_dir = main_data_dir
+        # Mappings
+        self.__postal_code_to_city = {}
+        self.__postal_code_addresses = {}  # {post code: [(street,unit,lat,long)....] }
+        self.__population_by_postal_code = {}  # {post code: (total pop, male pop, female pop)}
+        # Initiate random seed for generator functions
+        random.seed()
         # Load data
         self.load_postal_code_city()
         self.load_population_stats()
-        # Initiate random seed for generator functions
-        random.seed()
+
+    def get_postal_codes(self): return sorted(self.__population_by_postal_code.keys())
+
+    def get_city_by_postal_code(self, postal_code): return self.__postal_code_to_city[postal_code]
+
+    def get_population_by_postal_code(self, postal_code): return self.__population_by_postal_code[postal_code]
 
 
     def load_postal_code_city(self):
@@ -111,7 +113,7 @@ class FinnishAddressGenerator:
             for row in infile.readlines():
                 postcode = row[13:18] # Fixed width file
                 city_fi = row[179:199].strip()
-                self.postal_code_to_city[postcode] = city_fi
+                self.__postal_code_to_city[postcode] = city_fi
     
 
     def load_population_stats(self):
@@ -123,17 +125,17 @@ class FinnishAddressGenerator:
                 (postal_code_area, total_population, male_population, female_population) = row
                 postal_code = postal_code_area[0:5]
                 # Right now, this just uses total population
-                self.population_by_postal_code[postal_code] = int(total_population)
+                self.__population_by_postal_code[postal_code] = int(total_population)
         
 
     def load_postal_code_addresses(self, postal_code):
         # To save memory, this procedure loads addresses by the first number of a postal code.
         # If you call this procedure and the necessary posal code is already loaded - then it just does nothing
-        dict_length = len(self.postal_code_addresses.keys())
-        keys = list(self.postal_code_addresses.keys())
+        dict_length = len(self.__postal_code_addresses.keys())
+        keys = list(self.__postal_code_addresses.keys())
         if dict_length > 0 and keys[0][0] == postal_code[0]:
             return None
-        self.postal_code_addresses = {}
+        self.__postal_code_addresses = {}
         file_loc = os.sep.join((self.__main_data_dir, 'openaddr-collected-europe', 'fi', 'countrywide-fi.csv'))
         if not os.path.exists(file_loc):
             raise Exception('\n***You will need to download address files from openaddresses.io.  Please see the openaddr-collected-europe subdirectory for details.')
@@ -144,29 +146,29 @@ class FinnishAddressGenerator:
                 (long, lat,addr_number, street, unit, city, district, region, postcode, rowid, rowhash) = row
                 if postcode[0] == postal_code[0]:
                     # Add it to the dictionary
-                    if postcode not in self.postal_code_addresses:
-                        self.postal_code_addresses[postcode] = []
-                    self.postal_code_addresses[postcode].append((street, addr_number, long, lat))
+                    if postcode not in self.__postal_code_addresses:
+                        self.__postal_code_addresses[postcode] = []
+                    self.__postal_code_addresses[postcode].append((street, addr_number, long, lat))
 
 
-    def get_address_in_postal_code(self,postal_code):
+    def get_address_in_postal_code(self, postal_code):
         # The postal code population file is only good at 4-digit granularity
         self.load_postal_code_addresses(postal_code)
 
         potential_postal_codes = []
         potential_weights = [] # number of units per postal code
         
-        for pca_code in self.postal_code_addresses:
+        for pca_code in self.__postal_code_addresses:
             if pca_code[0:4] == postal_code[0:4]:
                 potential_postal_codes.append(pca_code)
-                potential_weights.append(len(self.postal_code_addresses[pca_code]))
+                potential_weights.append(len(self.__postal_code_addresses[pca_code]))
         
         chosen_code = random.choices(potential_postal_codes,weights=potential_weights)[0]
         
-        if chosen_code not in self.postal_code_addresses:
+        if chosen_code not in self.__postal_code_addresses:
             return ('','',25.0,66.5)
         else:
-            return random.choice(self.postal_code_addresses[chosen_code])
+            return random.choice(self.__postal_code_addresses[chosen_code])
     
 
 def get_arguments(args, arg_defs):
@@ -195,6 +197,12 @@ def main(argv):
 
     # Define and read program input arguments
     program_args = {
+        'sample_rate': {
+            'short_flag': 'r',
+            'type': float,
+            'default_value': .01,
+            'description': 'The sampling rate over the total population.'
+        },
         'data_dir': {
             'short_flag': 'd',
             'type': str,
@@ -221,27 +229,30 @@ def main(argv):
     log_level = getattr(logging, args.log_level.upper())
     logging.basicConfig(level=log_level, format=log_format)
 
-    fp = FinnishPersonGenerator(main_data_dir=args.data_dir)
-    fa = FinnishAddressGenerator(main_data_dir=args.data_dir)
-
-    people_per_postcode = fa.population_by_postal_code
-
-    postcode_keys = sorted(people_per_postcode.keys())
+    person_generator = FinnishPersonGenerator(main_data_dir=args.data_dir)
+    address_generator = FinnishAddressGenerator(main_data_dir=args.data_dir)
 
     output_file = os.sep.join((args.output_dir, output_filename))
     logging.info('Beginning output to ' + output_file)
+    total_samples = 0
     with open(output_file, mode='w', encoding='utf8', newline='') as ow: # change the newline if using Linux
         cw = csv.writer(ow)
-        for postcode in postcode_keys:
-            print('Processing {0}'.format(postcode))
-            population = people_per_postcode[postcode]
-            sample_rate = .05
-            for i in range(int(population * sample_rate)):
-                (first_name, last_name) = fp.get_a_name()
-                (street, unit, lat, long) = fa.get_address_in_postal_code(postcode)
-                city = fa.postal_code_to_city[postcode]
+        postal_codes = address_generator.get_postal_codes()
+        for postcode in postal_codes:
+            population = address_generator.get_population_by_postal_code(postcode)
+            num_samples = int(population * args.sample_rate)
+            logging.info('Processing postal code {0} ({1} samples)'.format(postcode, num_samples))
+            for _ in range(num_samples):
+                (first_name, last_name) = person_generator.get_a_name()
+                (street, unit, lat, long) = address_generator.get_address_in_postal_code(postcode)
+                city = address_generator.get_city_by_postal_code(postcode)
                 row = (first_name, last_name, street, unit, city, postcode, lat, long)
                 cw.writerow(row)
+            total_samples += num_samples
+    logging.info((
+        'Generated total {} samples for {} postal codes with sampling rate {:.4f}'
+        .format(total_samples, len(postal_codes), args.sample_rate)
+    ))
 
 
 if __name__ == '__main__':
